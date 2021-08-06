56fe1b9f22dcd7975f6b504b1037cb61238c5f73
JumbomanXDA <70740295+JumbomanXDA@users.noreply.github.com>
2021-08-06 08:39:16 +0530

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,7 +1,11 @@
 #!/system/bin/shsh
 
-while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 1; done
+# Wait till device boot process complets
+while [ "$(getprop sys.boot_completed)" != "1" ]; do
+	sleep 1
+done
 sleep 3
 
-# Mount vanced YouTube
+# mount Vanced YouTube with official YouTube
 chcon u:object_r:apk_data_file:s0 /data/adb/modules/VancedYT/vanced/base.apk
+
